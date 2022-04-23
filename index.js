83fd47dfbc4207f318b93e01c3df44831d09e39c
Luis Alberto Villegas Valenzuela <lualviva97@gmail.com>
2022-04-23 00:50:36 -0500

    Agregando el index al proyecto

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require("./app/missionCommander");
+const carlo = new MissionCommander("Carlo");
+console.log(carlo.name);
+const villegas = new MissionCommander("Villegas");
+console.log(villegas.name);
+const regi = new MissionCommander("Regis");
+console.log(regi.name);
